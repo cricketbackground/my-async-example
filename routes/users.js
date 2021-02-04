6ecdc803b00599bf608021ec66a639eef2bd65a1
cricketbackground <vadzz.java@gmail.com>
2021-02-04 18:57:08 -0500

    added async waterfall example

--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,20 @@ const express = require('express');
 const router = express.Router();
 const async = require('async');
 
-function myFirstFunction(first) {
-    first(null, 'Task 1', 'Task 2');
+function myFirstFunction(callback) {
+    callback(null, 'Task 1', 'Task 2');
 }
 
-function mySecondFunction(arg1, arg2, second) {
+function mySecondFunction(arg1, arg2, callback) {
     // arg1 now equals 'Task 1' and arg2 now equals 'Task 2'
     let arg3 = arg1 + ' and ' + arg2;
-    second(null, arg3);
+    callback(null, arg3);
 }
 
-function myLastFunction(arg1, third) {
+function myLastFunction(arg1, callback) {
     // arg1 now equals 'Task1 and Task2'
     arg1 += ' completed';
-    third(null, arg1);
+    callback(null, arg1);
 }
 
 /* GET users listing. */
